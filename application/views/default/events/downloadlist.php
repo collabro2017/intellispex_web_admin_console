594d772927c507414b3cd5ec36234718be35db53
Rasp Berry Frog <raspberryfrog1989@yahoo.com>
2017-08-04 16:20:36 +0500

    1019

--- application/views/default/events/downloadlist.php
+++ application/views/default/events/downloadlist.php
@@ -20,8 +20,8 @@
         <div class="span5"><br><br><br><br>
             <table>
                 <tr><td>Creator</td><td><?php echo $event[0]['username'] ?></td></tr>
-                <tr><td>Data Created</td><td><?php echo $event[0]['createdAt'] ?></td></tr>
-                <tr><td>Time Created</td><td><?php echo $event[0]['createdAt'] ?></td></tr>
+                <tr><td>Data Created</td><td><?php echo date('Y-m-d',strtotime($event[0]['createdAt'])); ?></td></tr>
+                <tr><td>Time Created</td><td><?php echo date('g:i A',strtotime($event[0]['createdAt'])); ?></td></tr>
                 <tr><td>Number of Participants</td><td><?php if(isset($event[0]['commenters'])) { echo count($event[0]['commenters']); }else{ echo 0; } ?></td></tr>
                 <tr><td>Tagged Users</td><td><?php echo count(implode(",",$event[0]['TagFriends'])) ?></td></tr>
                 <tr><td>Location</td><td><?php  ?></td></tr>
@@ -40,10 +40,10 @@
             <table>
                 <?php for($i = 0; $i < $halfActivity; $i++){ ?>
                     <tr><td>Activity Sheet <?php echo $i+1; ?></td><td>&nbsp;</td></tr>
-                    <tr><td>Date</td><td><?php echo date('Y-m-d',time($eventActivity[0]['createdAt'])); ?></td></tr>
-                    <tr><td>Time</td><td><?php echo date('g:i A',time($eventActivity[0]['createdAt'])); ?></td></tr>
-                    <tr><td>Title</td><td><?php echo $eventActivity[0]['title']; ?></td></tr>
-                    <tr><td>Desctiption</td><td><?php echo $eventActivity[0]['description']; ?></td></tr>
+                    <tr><td>Date</td><td><?php echo date('Y-m-d',strtotime($eventActivity[$i]['createdAt'])); ?></td></tr>
+                    <tr><td>Time</td><td><?php echo date('g:i A',strtotime($eventActivity[$i]['createdAt'])); ?></td></tr>
+                    <tr><td>Title</td><td><?php echo $eventActivity[$i]['title']; ?></td></tr>
+                    <tr><td>Desctiption</td><td><?php echo $eventActivity[$i]['description']; ?></td></tr>
                 <?php } ?>
             </table>
             <?php 
@@ -55,10 +55,10 @@
             <table>
                 <?php for($i = $halfActivity; $i < count($eventActivity); $i++){ ?>
                     <tr><td>Activity Sheet <?php echo $i+1; ?></td><td>&nbsp;</td></tr>
-                    <tr><td>Date</td><td><?php echo date('Y-m-d',time($eventActivity[0]['createdAt'])); ?></td></tr>
-                    <tr><td>Time</td><td><?php echo date('g:i A',time($eventActivity[0]['createdAt'])); ?></td></tr>
-                    <tr><td>Title</td><td><?php echo $eventActivity[0]['title']; ?></td></tr>
-                    <tr><td>Desctiption</td><td><?php echo $eventActivity[0]['description']; ?></td></tr>
+                    <tr><td>Date</td><td><?php echo date('Y-m-d',time($eventActivity[$i]['createdAt'])); ?></td></tr>
+                    <tr><td>Time</td><td><?php echo date('g:i A',time($eventActivity[$i]['createdAt'])); ?></td></tr>
+                    <tr><td>Title</td><td><?php echo $eventActivity[$i]['title']; ?></td></tr>
+                    <tr><td>Desctiption</td><td><?php echo $eventActivity[$i]['description']; ?></td></tr>
                 <?php } ?>
             </table>
             <?php } ?>
