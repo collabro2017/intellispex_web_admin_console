af7d3485b926625519c2adbd9e5b6b44e409103d
Snow4Sandra <collabro2016@hotmail.com>
2017-10-05 18:49:24 +0800

    change base url for test site

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://intellispex.com/';
+$config['base_url']	= 'http://test.intellispex.com/';
 
 /*
 |--------------------------------------------------------------------------
