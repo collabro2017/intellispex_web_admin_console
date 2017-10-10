db41a19882f5bb289f667b2c796028a133d2b94f
Rasp Berry Frog <raspberryfrog1989@yahoo.com>
2017-10-10 10:02:25 +0500

    1020

--- index.php
+++ index.php
@@ -27,6 +27,8 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
+        
+date_default_timezone_set('America/Chicago');
 
 if (defined('ENVIRONMENT'))
 {
