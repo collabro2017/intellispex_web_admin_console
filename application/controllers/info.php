c6c12182512354d45248c93e4fbae44181ce3495
Muzammil <muzammil.qureshi@gmail.com>
2017-11-15 19:30:58 +0500

    Privacy Page

---
+++ application/controllers/info.php
@@ -0,0 +1,27 @@
+<?php
+
+ini_set('session.cache_limiter', 'public');
+session_cache_limiter(false);
+session_start();
+include_once('CI_Controller_EX.php');
+
+class info extends CI_Controller_EX {
+
+    public function __construct() {
+        parent::__construct();
+        $this->load->helper('url');
+        $this->load->model('M_user', '', TRUE);
+        $this->load->model('M_client', '', TRUE);
+        if ($this->input->post('remember_me')) // set sess_expire_on_close to 0 or FALSE when remember me is checked.
+            $this->config->set_item('sess_expire_on_close', '0'); // do change session config
+        $this->load->library("session");
+        $this->load->library('form_validation');
+        $this->load->library('grocery_CRUD');
+        $this->load->library('ParseRestClient');
+    }
+
+    public function privacy() {
+        $this->load->view('default/privacy');
+    }
+}
+    ?>
